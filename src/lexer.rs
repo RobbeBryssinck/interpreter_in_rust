e89735bd027dc97ded1c59e79c707f3f40bf8bc7
Robbe Bryssinck <robbebryssinck@gmail.com>
2021-06-27 00:54:06 +0200

    Lexing of keywords, variables, integers

--- src/lexer.rs
+++ src/lexer.rs
@@ -9,8 +9,8 @@ pub enum TokenParsingError {
 
 struct Lexer {
     input: String,
-    position: i32,
-    read_position: i32,
+    position: usize,
+    read_position: usize,
     character: char
 }
 
@@ -28,81 +28,287 @@ impl Lexer {
         lexer
     }
 
+    // TODO: Error or illegal token?
     pub fn next_token(&mut self) -> Result<Token, TokenParsingError> {
+        self.skip_whitespace();
+
         let result = match self.character {
-            '=' => Ok(Token::new(String::from(token::ASSIGN), String::from(self.character))),
-            ';' => Ok(Token::new(String::from(token::SEMICOLON), String::from(self.character))),
-            '(' => Ok(Token::new(String::from(token::LPAREN), String::from(self.character))),
-            ')' => Ok(Token::new(String::from(token::RPAREN), String::from(self.character))),
-            ',' => Ok(Token::new(String::from(token::COMMA), String::from(self.character))),
-            '+' => Ok(Token::new(String::from(token::PLUS), String::from(self.character))),
-            '{' => Ok(Token::new(String::from(token::LBRACE), String::from(self.character))),
-            '}' => Ok(Token::new(String::from(token::RBRACE), String::from(self.character))),
-            '\x00' => Ok(Token::new(String::from(token::EOF), String::from(""))),
-            _ => Err(TokenParsingError::UnknownToken)
+            '=' => Ok(Token::new(String::from(token::ASSIGN), String::from(self.character), false)),
+            ';' => Ok(Token::new(String::from(token::SEMICOLON), String::from(self.character), false)),
+            '(' => Ok(Token::new(String::from(token::LPAREN), String::from(self.character), false)),
+            ')' => Ok(Token::new(String::from(token::RPAREN), String::from(self.character), false)),
+            ',' => Ok(Token::new(String::from(token::COMMA), String::from(self.character), false)),
+            '+' => Ok(Token::new(String::from(token::PLUS), String::from(self.character), false)),
+            '{' => Ok(Token::new(String::from(token::LBRACE), String::from(self.character), false)),
+            '}' => Ok(Token::new(String::from(token::RBRACE), String::from(self.character), false)),
+            '\x00' => Ok(Token::new(String::from(token::EOF), String::from(""), false)),
+            _ if self.is_letter() => {
+                let identifier = self.read_identifier();
+                Ok(Token::new(String::from(Token::lookup_identifier(identifier)), String::from(identifier), true))
+            },
+            _ if self.character.is_digit(10) => {
+                Ok(Token::new(String::from(token::INT), String::from(self.read_number()), true))
+            },
+            _ => Err(TokenParsingError::UnknownToken),
         };
 
+        let result = match result {
+            Ok(result) if result.is_identifier => return Ok(result),
+            _ => result
+        };
+
+        println!("About to read...");
         self.read_char();
 
         result
     }
 
     fn read_char(&mut self) {
-        if self.read_position >= self.input.chars().count() as i32 {
+        println!("Reading..");
+        if self.read_position >= self.input.chars().count() {
             self.character = '\x00';
         } else {
-            self.character = self.input.chars().nth(self.read_position as usize).expect("Cannot read character.");
+            self.character = self.input.chars().nth(self.read_position).expect("Cannot read character.");
         }
         self.position = self.read_position;
         self.read_position += 1;
+    }
+
+    fn read_identifier(&mut self) -> &str {
+        let position = self.position;
+
+        println!("Reading identifier...");
+        while self.is_letter() {
+            self.read_char();
+        }
+        println!("Done reading identifier: {}", &self.input[position as usize..self.position as usize]);
+
+        &self.input[position as usize..self.position as usize]
+    }
+
+    fn read_number(&mut self) -> &str {
+        let position = self.position;
+
+        while self.character.is_digit(10) {
+            self.read_char();
+        }
+
+        &self.input[position as usize..self.position as usize]
+    }
+
+    fn is_letter(&self) -> bool {
+        self.character.is_alphabetic() || self.character == '_'
+    }
+
+    fn skip_whitespace(&mut self) {
+        while self.character == ' ' || self.character == '\t' || self.character == '\n' || self.character == '\r' {
+            self.read_char();
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::token::{FUNCTION, IDENT, INT, LET};
+
     use super::token::{Token, ASSIGN, PLUS, LPAREN, RPAREN, LBRACE, RBRACE, COMMA, SEMICOLON, EOF};
     use super::*;
     
 	#[test]
 	fn next_token_test() {
-        let input = String::from("=+(){},;");
+        let input = String::from("let five = 5;
+let ten = 10;
+let add = fn(x, y) {
+x + y;
+};
+let result = add(five, ten);
+");
 
-        let test_tokens: [Token; 9] = [
+        let test_tokens = [
+            Token {
+                token_type: String::from(LET),
+                literal: String::from("let"),
+                is_identifier: true,
+            },
+            Token {
+                token_type: String::from(IDENT),
+                literal: String::from("five"),
+                is_identifier: true,
+            },
             Token {
                 token_type: String::from(ASSIGN),
                 literal: String::from("="),
+                is_identifier: false,
             },
             Token {
-                token_type: String::from(PLUS),
-                literal: String::from("+"),
-            },
-            Token {
-                token_type: String::from(LPAREN),
-                literal: String::from("("),
-            },
-            Token {
-                token_type: String::from(RPAREN),
-                literal: String::from(")"),
-            },
-            Token {
-                token_type: String::from(LBRACE),
-                literal: String::from("{"),
-            },
-            Token {
-                token_type: String::from(RBRACE),
-                literal: String::from("}"),
-            },
-            Token {
-                token_type: String::from(COMMA),
-                literal: String::from(","),
+                token_type: String::from(INT),
+                literal: String::from("5"),
+                is_identifier: false,
             },
             Token {
                 token_type: String::from(SEMICOLON),
                 literal: String::from(";"),
+                is_identifier: false,
+            },
+            Token {
+                token_type: String::from(LET),
+                literal: String::from("let"),
+                is_identifier: true,
+            },
+            Token {
+                token_type: String::from(IDENT),
+                literal: String::from("ten"),
+                is_identifier: true,
+            },
+            Token {
+                token_type: String::from(ASSIGN),
+                literal: String::from("="),
+                is_identifier: false,
+            },
+            Token {
+                token_type: String::from(INT),
+                literal: String::from("10"),
+                is_identifier: false,
+            },
+            Token {
+                token_type: String::from(SEMICOLON),
+                literal: String::from(";"),
+                is_identifier: false,
+            },
+            Token {
+                token_type: String::from(LET),
+                literal: String::from("let"),
+                is_identifier: true,
+            },
+            Token {
+                token_type: String::from(IDENT),
+                literal: String::from("add"),
+                is_identifier: true,
+            },
+            Token {
+                token_type: String::from(ASSIGN),
+                literal: String::from("="),
+                is_identifier: false,
+            },
+            Token {
+                token_type: String::from(FUNCTION),
+                literal: String::from("fn"),
+                is_identifier: true,
+            },
+            Token {
+                token_type: String::from(LPAREN),
+                literal: String::from("("),
+                is_identifier: false,
+            },
+            Token {
+                token_type: String::from(IDENT),
+                literal: String::from("x"),
+                is_identifier: true,
+            },
+            Token {
+                token_type: String::from(COMMA),
+                literal: String::from(","),
+                is_identifier: false,
+            },
+            Token {
+                token_type: String::from(IDENT),
+                literal: String::from("y"),
+                is_identifier: true,
+            },
+            Token {
+                token_type: String::from(RPAREN),
+                literal: String::from(")"),
+                is_identifier: false,
+            },
+            Token {
+                token_type: String::from(LBRACE),
+                literal: String::from("{"),
+                is_identifier: false,
+            },
+            Token {
+                token_type: String::from(IDENT),
+                literal: String::from("x"),
+                is_identifier: true,
+            },
+            Token {
+                token_type: String::from(PLUS),
+                literal: String::from("+"),
+                is_identifier: false,
+            },
+            Token {
+                token_type: String::from(IDENT),
+                literal: String::from("y"),
+                is_identifier: true,
+            },
+            Token {
+                token_type: String::from(SEMICOLON),
+                literal: String::from(";"),
+                is_identifier: false,
+            },
+            Token {
+                token_type: String::from(RBRACE),
+                literal: String::from("}"),
+                is_identifier: false,
+            },
+            Token {
+                token_type: String::from(SEMICOLON),
+                literal: String::from(";"),
+                is_identifier: false,
+            },
+            Token {
+                token_type: String::from(LET),
+                literal: String::from("let"),
+                is_identifier: true,
+            },
+            Token {
+                token_type: String::from(IDENT),
+                literal: String::from("result"),
+                is_identifier: true,
+            },
+            Token {
+                token_type: String::from(ASSIGN),
+                literal: String::from("="),
+                is_identifier: false,
+            },
+            Token {
+                token_type: String::from(IDENT),
+                literal: String::from("add"),
+                is_identifier: true,
+            },
+            Token {
+                token_type: String::from(LPAREN),
+                literal: String::from("("),
+                is_identifier: false,
+            },
+            Token {
+                token_type: String::from(IDENT),
+                literal: String::from("five"),
+                is_identifier: true,
+            },
+            Token {
+                token_type: String::from(COMMA),
+                literal: String::from(","),
+                is_identifier: false,
+            },
+            Token {
+                token_type: String::from(IDENT),
+                literal: String::from("ten"),
+                is_identifier: true,
+            },
+            Token {
+                token_type: String::from(RPAREN),
+                literal: String::from(")"),
+                is_identifier: false,
+            },
+            Token {
+                token_type: String::from(SEMICOLON),
+                literal: String::from(";"),
+                is_identifier: false,
             },
             Token {
                 token_type: String::from(EOF),
                 literal: String::from(""),
+                is_identifier: false,
             },
         ];
 
--- src/token.rs
+++ src/token.rs
@@ -1,12 +1,22 @@
+use phf::phf_map;
+
 // TODO: string for token type might not be performant
 pub struct Token {
     pub token_type: String,
     pub literal: String,
+    pub is_identifier: bool,
 }
 
 impl Token {
-    pub fn new(token_type: String, literal: String) -> Self {
-        Token { token_type, literal }
+    pub fn new(token_type: String, literal: String, is_identifier: bool) -> Self {
+        Token { token_type, literal, is_identifier }
+    }
+
+    pub fn lookup_identifier(identifier: &str) -> &str {
+        match KEYWORDS.get(identifier) {
+            Some(keyword) => keyword,
+            _ => IDENT
+        }
     }
 }
 
@@ -33,3 +43,8 @@ pub const RBRACE: &str = "}";
 // Keywords
 pub const FUNCTION: &str = "FUNCTION";
 pub const LET: &str = "LET";
+
+pub static KEYWORDS: phf::Map<&'static str, &str> = phf_map! {
+    "fn" => FUNCTION,
+    "let" => LET,
+};
